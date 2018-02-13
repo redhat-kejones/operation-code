28c182b3ff9dd33c29392019af2b6c55a5e96d75
Kevin Jones <kejones@kejones-travel.usersys.redhat.com>
2018-02-13 15:03:51 -0500

    Change type to text/html

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var http = require('http');
 var datetime = new Date();
     console.log(datetime);
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<h1>Hello World</h1>\n<h2>'+datetime+'</h2>\n');
 }).listen(8080, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:8080/');

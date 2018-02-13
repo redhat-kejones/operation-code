95d6bae00c5f9a852c3a268bc7db1bcf48df2846
Kevin Jones <kejones@kejones-travel.usersys.redhat.com>
2018-02-13 15:18:32 -0500

    Move date code into execution function for update on reload

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var http = require('http');
-var datetime = new Date();
-    console.log(datetime);
 http.createServer(function (req, res) {
+  var datetime = new Date();
+      console.log(datetime);
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<h1>Hello World</h1>\n<h2>'+datetime+'</h2>\n');
 }).listen(8080, '127.0.0.1');
